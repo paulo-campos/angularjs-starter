8ba306fc668ba6def56c2b5cf6fdabe13a8778a8
Paulo Campos <paulovitorwd@gmail.com>
2016-07-04 23:27:38 -0300

    Updating config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 sass_dir         = 'dev/scss'
 css_dir          = 'dev/assets/stylesheets'
-images_dir       = 'dev/assets/images'
+images_dir       = 'dev/assets/images/app'
 fonts_dir        = 'dev/assets/fonts'
 relative_assets  = true
 line_comments    = true
